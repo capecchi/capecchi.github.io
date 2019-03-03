e2d746e2ddeffd547ed8d83ae05ff3e4d8512825
capecchi <william.capecchi@gmail.com>
2019-03-03 16:43:44 -0600

    starting path-zipping idea

--- projects/ParkMaps/create_park_coords.py
+++ projects/ParkMaps/create_park_coords.py
@@ -58,14 +58,39 @@ def smooth_park_runs_coords(run_dict: dict):
     return smooth_run_dict
 
 
+def another_run_nearby(coord, current_run_key, run_dict, nearby_radius_m):
+    nearby_run_icoord = {}
+    for key, rc in run_dict.items():
+        if key != current_run_key:
+            dis = np.array([dist.vincenty(coord[::-1][1:], rc[i, ::-1][1:]).m for i in np.arange(len(rc))])
+            if min(dis) <= nearby_radius_m:  # another run is sufficiently nearby
+                nearby_run_icoord[key] = np.where(dis == min(dis))[0][0]
+    return nearby_run_icoord
+
+
+def identify_junctions(run_dict: dict, bubble_radius_m=500, nearby_radius_m=10):
+    for key, run_coords in run_dict.items():
+        for coord in run_coords:
+            nearby_runs_icoord = another_run_nearby(coord, key, run_dict, nearby_radius_m)
+            if len(nearby_runs_icoord) > 0:  # nearby run found
+                # given runs nearby A, B, C.
+                # create a, a subset of A where a starts from coord and extends until it is no longer near B, C
+                # create b, s.t. b is close to a
+                # create c, s.t. c is close to b
+                # recreate a, b s.t. a, b are close to c
+                # except for the first step these should all be very fast since arrays become much shorter right away
+            a=1
+
+
 def plot_runs(runs: dict, ax):
     for run in runs.keys():
-        ax.plot(runs[run][:, 0], runs[run][:, 1], '-')
+        ax.plot(runs[run][:, 0], runs[run][:, 1], 'o-')
 
 
 if __name__ == "__main__":
     park = 'ElmCreekRuns/'
     rd = extract_park_runs_coords(park)
+    juncs = identify_junctions(rd)
     # srd = smooth_park_runs_coords(rd)
     fig = plt.figure('Park Runs')
     ax = fig.add_subplot(111)
