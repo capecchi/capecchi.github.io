6f3628e38b9b19fc73d89c2ac117b226a00962f2
wcapecch <william.capecchi@gmail.com>
2021-11-24 16:04:55 -0500

    race and plot options on strava landing page with pre-checked values

--- posts/RaceTraining/test_app.py
+++ posts/RaceTraining/test_app.py
@@ -1,6 +1,9 @@
+import datetime
+
 from bottle import run, jinja2_view, get, post, request
-from wtforms import Form, SubmitField, SelectMultipleField, widgets, StringField, HiddenField
+from wtforms import Form, SubmitField, SelectMultipleField, widgets
 from posts.RaceTraining.get_strava_data import get_past_races
+
 
 class MultiCheckboxField(SelectMultipleField):
     widget = widgets.ListWidget(prefix_label=False)
@@ -9,25 +12,38 @@ class MultiCheckboxField(SelectMultipleField):
 
 class MyForm(Form):
     races = get_past_races()
+    races.update({'Past 18 weeks': datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)})
     race_options = [(r, r) for r in races.keys()]
-    runs = MultiCheckboxField(choices=race_options)
+    runs = MultiCheckboxField(label='Race Options', choices=race_options)
     docode = SubmitField('Run Analysis')
+    plot_options = [('rdist', 'Distance vs. Days Before'), ('rcum', 'Cumulative Distance vs. Days Before'),
+                    ('rwk', 'Current Week'), ('rpace', 'Pace vs. Days Before'),
+                    ('rcal', 'Calories (cumulative) vs. Days Before'), ('rsvd', 'Speed vs. Distance'),
+                    ('rswt', 'Sweat Rate Histogram'),
+                    ('calbytype', 'Calories (cumulative) by Activity Type over past 18 weeks')]
+    plots = MultiCheckboxField(choices=plot_options)
 
 
 port = 8000
 
 
+def do_prepopulate(form: MyForm):
+    form.runs.data = ['Past 18 weeks']
+    form.plots.data = ['rswt', 'calbytype']
+
+
 @post('/')
-@jinja2_view('home.html')
+@jinja2_view('test_app.html')
 def submit():
     form = MyForm(request.forms)
     return {'form': form}
 
 
 @get('/')
-@jinja2_view('home.html')
+@jinja2_view('test_app.html')
 def home():
     form = MyForm()
+    do_prepopulate(form)
     return {'form': form}
     # '''
     # <form action="/" method="post">
